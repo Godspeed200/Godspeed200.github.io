da96fdccd4d25c009badb1df66a7ad5b9a0b7552
Godspeed200 <80467105+Godspeed200@users.noreply.github.com>
2026-06-23 13:36:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,3 @@ function veranderTekst() {
     document.getElementById("text").innerText =
         "System running at 100% performance ✅";
 }
-
-function kleurVeranderen() {
-    document.body.style.background =
-        "radial-gradient(circle at top, #1f4037, #000000)";
-}
